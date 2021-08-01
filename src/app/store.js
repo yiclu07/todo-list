ca371476c9d8f8f559c18743a6846057a4d910d0
i-love-sana <yi.c.lu07@gmail.com>
2021-08-01 14:53:10 -0400

    Add taskList reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import taskListReducer from '../features/taskList/taskListSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    taskList: taskListReducer
   },
 });
