8ca60f16a615005c5a8c938b32df5cf1f59da144
i-love-sana <yi.c.lu07@gmail.com>
2021-08-01 12:59:16 -0400

    Initial taskListSLice code

--- src/features/taskList/taskListSlice.js
+++ src/features/taskList/taskListSlice.js
@@ -1,3 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  tasks: [
+    /* {
+      task: '',
+      id: 0
+    } */
+  ]
+};
+
+export const taskListSlice = createSlice({
+  name: 'taskList',
+  initialState,
+  reducers: {
+    addNewTask: (state, action) => {
+      state.tasks.push(action.payload);
+    },
+    deleteTask: (state, action) => {
+      state.tasks.filter(task.id !== action.payload.id);
+    }
+  }
+});
+
+export const { addNewTask, deleteTask } = taskListSlice.actions;
+export default taskListSlice.reducer;
