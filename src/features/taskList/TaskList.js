734bdaf148cc516bea1fe1dfcf8869667ed9c1e2
i-love-sana <yi.c.lu07@gmail.com>
2021-08-01 13:26:44 -0400

    Initial TaskList code

--- src/features/taskList/TaskList.js
+++ src/features/taskList/TaskList.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import {
+  addNewTask,
+  deleteTask
+} from './taskListSlice.js';
+import styles from './TaskList.module.css';
 
 export function TaskList() {
+  const dispatch = useDispatch();
+
   return (
     <div>
-
+      <h2>Today's Tasks</h2>
+      <div>
+        {/* List of tasks */}
+      </div>
     </div>
   );
 }
