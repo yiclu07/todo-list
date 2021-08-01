fb9a658136e3987b6351e1553e6f2881df72057b
i-love-sana <yi.c.lu07@gmail.com>
2021-08-01 15:45:53 -0400

    Functionality to add new task

--- src/features/taskList/TaskList.js
+++ src/features/taskList/TaskList.js
@@ -9,12 +9,33 @@ import styles from './TaskList.module.css';
 export function TaskList() {
   const dispatch = useDispatch();
 
+  /* task will store a string that corresponds to the user's input and will be used to add new tasks to TaskList */
+  const [task, setTask] = useState('');
+
+  const handleTaskChange = (e) => {
+    setTask(e.target.value);
+  }
+
+  const handleAddTask = (e) => {
+    /* Since a form will likely be used to handle adding tasks to TaskList, e.preventDefault() will prevent the default action of a form refreshing the browser on submit */
+    e.preventDefault();
+
+    dispatch(addNewTask(task));
+
+    /* Clears the input on submit */
+    setTask('');
+
+  }
+
   return (
     <div>
       <h2>Today's Tasks</h2>
       <div>
         {/* List of tasks */}
       </div>
+      <form onSubmit={handleAddTask}>
+        <input value={task} onChange={handleTaskChange} placeholder="Add new task..." />
+      </form>
     </div>
   );
 }
